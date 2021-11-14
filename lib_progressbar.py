fff4ad6582c8cdfe0acab925dfbbfbd13f77d5a6
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-14 18:19:39 +0100

    Add files via upload

--- lib_progressbar.py
+++ lib_progressbar.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 # local Libaries
 from lib_time import chhop_microseconds
 
-def progress_bar(item_iterable: Iterable, bar_length: int = 60, prefix: str = 'PROCESSING: ') -> Generator:
+
+def progress_bar(item_iterable, bar_length: int = 20, prefix: str = 'PROCESSING: ') -> Generator:
     """
     Usage Example:
     for i in progress_bar(range(100)):
@@ -16,13 +17,19 @@ def progress_bar(item_iterable: Iterable, bar_length: int = 60, prefix: str = 'P
     :param bar_length: std = 60
     :return:
     """
+    if not item_iterable:
+        return
+
     start_time = datetime.now()
     item_count = len(item_iterable)
-    for index, item in enumerate(item_iterable):
-        index += 1  # add offset to index
+
+    if item_count == 0:
+        return
+
+    def update():
         bar_progress = int(bar_length * index / item_count)
         runtime = datetime.now() - start_time
-        remaining_time = max(runtime / index * item_count - runtime, timedelta(seconds=0))
+        remaining_time = max(runtime / max(index - 1, 1) * item_count - runtime, timedelta(seconds=0))
         string = str(f'\r{prefix}'
                      f'[{"#" * bar_progress}{"." * (bar_length - bar_progress)}] '
                      f'{index}/{item_count} '
@@ -30,6 +37,10 @@ def progress_bar(item_iterable: Iterable, bar_length: int = 60, prefix: str = 'P
                      f'Remaining: {chhop_microseconds(remaining_time)}')
         sys.stdout.write(string)
         sys.stdout.flush()
+
+    for index, item in enumerate(item_iterable, start=1):
+        update()
         yield item
+
     sys.stdout.write(" - Done! \n")
     sys.stdout.flush()
